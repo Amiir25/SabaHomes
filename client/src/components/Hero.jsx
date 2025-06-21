0c191823e952bbc314bba4d6f5ff3fa1d9e3ceee
Amiir25 <amirsadik636@gmail.com>
2025-06-21 20:44:39 +0300

    Create Hero.jsx

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <>
+        <div className="px-6 md:px-16 lg:px-24 xl:px-32 border
+        bg-[url('/src/assets/hero-1.png')] bg-cover bg-center bg-no-repeat h-screen">
+            <p className="bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20">The Ultimate Hotel Experiance</p>
+        </div>
+        </>
+    )
+}
+
+export default Hero;
