0ba2ef2d86af7e516898afad2d79a30291dc1a92
Amiir25 <amirsadik636@gmail.com>
2025-06-22 17:04:59 +0300

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <>
         <div className="flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 border
-        bg-[url('/src/assets/hero-4.jpg')] bg-cover bg-center bg-no-repeat h-screen">
+        bg-[url('/src/assets/hero.jpg')] bg-cover bg-center bg-no-repeat h-screen">
             
         </div>
         </>
