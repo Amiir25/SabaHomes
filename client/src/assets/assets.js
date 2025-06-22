24d483b63338023246a26ed7e46bc362aefc828c
Amiir25 <amirsadik636@gmail.com>
2025-06-22 17:06:12 +0300

    Create assets.js

---
+++ client/src/assets/assets.js
@@ -0,0 +1,13 @@
+import logo from './logo.png';
+import hero from './hero.jpg';
+import searchIcon from './searchIcon.svg';
+import menuIcon from './menuIcon.svg';
+import closeIcon from './closeIcon.svg';
+
+export const assets = {
+    logo,
+    hero,
+    searchIcon,
+    menuIcon,
+    closeIcon,
+}
