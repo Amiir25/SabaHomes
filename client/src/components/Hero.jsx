4f478520a51193b7c484ca86adb93f18467be62f
Amiir25 <amirsadik636@gmail.com>
2025-06-23 18:09:38 +0300

    Update Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,11 +4,11 @@ const Hero = () => {
     return (
         <>
             <div className="bg-[url('/src/assets/hero-10.jpg')] bg-cover bg-center bg-no-repeat h-screen">
-                <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 w-full h-screen bg-[rgba(255,255,255,0.2)] text-gray-900">
+                <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 w-full h-screen bg-[rgba(255,255,255,0.5)] text-gray-900">
                     
                     <div className="mt-50">
-                        <p className="md:text-2xl font-black text-center">Homes for Every Stage of Life</p>
-                        <h1 className="md:text-5xl font-light mt-2 max-w-xl text-center">
+                        <p className="md:text-3xl font-black text-center">Homes for Every Stage of Life</p>
+                        <h1 className="md:text-6xl font-light mt-2 max-w-3xl text-center">
                             Find a place that reflects who you are!
                         </h1>
                         {/* <p className="max-w-130 mt-2 text-sm md:text-base">
