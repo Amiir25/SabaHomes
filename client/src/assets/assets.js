8b9407cee15bf0ce6023c5c50c99217bfd6cd53c
Amiir25 <amirsadik636@gmail.com>
2025-06-23 18:06:31 +0300

    Update assets.js

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,7 @@
 import logo from './logo.png';
+import logoDark from './logo-dark.png';
+import logoGreen from './logo-green.png';
+import logoPink from './logo-pink.png';
 import hero from './hero.jpg';
 import searchIcon from './searchIcon.svg';
 import menuIcon from './menuIcon.svg';
@@ -6,6 +9,9 @@ import closeIcon from './closeIcon.svg';
 
 export const assets = {
     logo,
+    logoDark,
+    logoGreen,
+    logoPink,
     hero,
     searchIcon,
     menuIcon,
