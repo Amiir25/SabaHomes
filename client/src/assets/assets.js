12f640c17d90b5bb7f694e19b1a7838a1b6fa7d1
Amiir25 <amirsadik636@gmail.com>
2025-06-24 15:42:55 +0300

    Update assets.js

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,7 @@
 import calenderIcon from './calenderIcon.svg';
 import closeIcon from './closeIcon.svg';
+
+// Logo
 import hero from './hero.jpg';
 import logo from './logo.png';
 import logoDark from './logo-dark.png';
@@ -8,10 +10,25 @@ import logoPink from './logo-pink.png';
 import menuIcon from './menuIcon.svg';
 import searchIcon from './searchIcon.svg';
 
+// Houses
+import house1 from './houses/house-1.jpg';
+import house2 from './houses/house-2.jpg';
+import house3 from './houses/house-3.jpg';
+import house4 from './houses/house-4.jpg';
+import house5 from './houses/house-5.jpg';
+import house6 from './houses/house-6.jpg';
+
+
 export const assets = {
     calenderIcon,
     closeIcon,
     hero,
+    house1,
+    house2,
+    house3,
+    house4,
+    house5,
+    house6,
     logo,
     logoDark,
     logoGreen,
@@ -20,12 +37,58 @@ export const assets = {
     searchIcon,
 }
 
+// Locations
 export const locations = [
     "Mexico",
-    "Sarbet",
-    "Lideta",
-    "Kasanchis",
+    "Gerji",
     "Lebu",
-    "CMC",
-    "Bethel"
+    "Piassa",
+    "Summit",
+    "Bethel",
+    "Kazanchis",
+    "Megenagna",
+];
+
+// Properties
+export const houses = [
+    {
+        "_id": "Ger00AD1",
+        "type": "Villa",
+        "location": "Gerji",
+        "price": "25,240,000",
+        "bedRoom": 3,
+        "bathRoom": 2,
+        "size": "1587 sq ft",
+        "image": house2,
+    },
+    {
+        "_id": "Bet00A14",
+        "type": "Villa",
+        "location": "Bethel",
+        "price": "32,450,000",
+        "bedRoom": 4,
+        "bathRoom": 3,
+        "size": "1587 sq ft",
+        "image": house5,
+    },
+    {
+        "_id": "sum0BAD4",
+        "type": "Villa",
+        "location": "Summit",
+        "price": "23,250,000",
+        "bedRoom": 2,
+        "bathRoom": 2,
+        "size": "1587 sq ft",
+        "image": house4,
+    },
+    {
+        "_id": "leb00AD1",
+        "type": "Villa",
+        "location": "Lebu",
+        "price": "25,870,000",
+        "bedRoom": 4,
+        "bathRoom": 2,
+        "size": "1587 sq ft",
+        "image": house3,
+    },
 ];
