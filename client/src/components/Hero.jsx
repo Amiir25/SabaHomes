a7054f39930c45bfc4003833d284e7b303e2a48b
Amiir25 <amirsadik636@gmail.com>
2025-09-25 21:52:19 +0300

    Make the bg image dynamic on the hero section in Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,36 +5,47 @@ const Hero = () => {
 
     // Images for dynamic background
     const bgImages = [
-        'bg-6.png', 'bg-8.png', 'bg-9.jpg', 'bg-10.jpg'
+        'bg-1.jpg', 'bg-2.jpg', 'bg-3.jpg', 'bg-4.jpg', 'bg-5.jpg',
+        'bg-7.png', 'bg-8.png', 'bg-9.jpg', 'bg-10.jpg',
+        'bg-12.jpg', 'bg-13.jpg', 'bg-14.jpg',
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentIndex(prevIndex => 
-                prevIndex === bgImages.length - 1 ? 0 : prevIndex + 1
-            )
+            setCurrentIndex(prev => (prev + 1) % bgImages.length);
         }, 5000);
+
+        return () => clearInterval(interval);
     }, [bgImages.length]);
     return (
         <>
-            <div style={{ backgroundImage: `url(${bgImages[currentIndex]})` }} className='bg-cover bg-center bg-no-repeat h-screen'>
-                <div className="flex flex-col items-left gap-10 px-6 md:px-16 lg:px-24 xl:px-32 w-full h-screen
-                bg-gradient-to-r from-white/95 via-white/90 to-white/0 text-gray-900">
+            <div className='relative w-screen h-screen overflow-x-scroll'>
+                {/* Sliding image container */}
+                {
+                    bgImages.map((image, i) => (
+                        <div style={{ backgroundImage: `url(${image})` }}
+                        className={`absolute inset-0 bg-cover bg-center bg-no-repeat transition-opacity duration-3000
+                        ${ i === currentIndex ? 'opacity-100' : 'opacity-0' }`}></div>
+                    ))
+                }
 
-                    <div className="mt-45 w-fit text-center">
+                <div className="relative z-10 px-6 md:px-16 lg:px-24 xl:px-32 w-screen h-screen
+                    bg-gradient-to-r from-white/95 via-white/80 to-white/0 text-gray-900">
+                    <div className="pt-45 w-fit text-center">
                         {/* <p className="md:text-3xl font-black">Homes for Every Stage of Life</p> */}
                         <h1 className="md:text-7xl font-light mt-2 max-w-3xl [text-shadow:4px_4px_12px_rgba(0,0,0,0.8)]">
                             Find Your Perfect Home in Addis Ababa!
                         </h1>
                         <p className="max-w-2xl mt-4 text-3xl text-center mx-auto [text-shadow:1px_1px_2px_rgba(0,0,0,0.4)] tracking-wider">
-                           SabaHomes connects you to the finest apartments, villas, and offices across Addis Ababa.
+                        SabaHomes connects you to the finest apartments, villas, and offices across Addis Ababa.
                         </p>
                     </div>
 
                     {/* Form */}
-                    <form className='bg-[rgba(255,255,255,0.8)] border text-gray-900 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
+                    <form className='w-fit mt-6 bg-[rgba(255,255,255,0.8)] border text-gray-900 rounded-lg px-6 py-4 
+                    flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
 
                         <div>
                             <div className='flex items-center gap-2'>
@@ -62,14 +73,6 @@ const Hero = () => {
 
                         <div>
                             <div className='flex items-center gap-2'>
-                                <label htmlFor="propertyStatus">Property Status</label>
-                            </div>
-                            <input id="propertyStatus" type=""
-                                className=" rounded border border-gray-400 px-2 py-1.5 mt-1.5 text-sm outline-none" />
-                        </div>
-
-                        <div>
-                            <div className='flex items-center gap-2'>
                                 <label htmlFor="priceLimit">Price Limit</label>
                             </div>
                             <input id="priceLimit" type=""
@@ -82,7 +85,6 @@ const Hero = () => {
                             <span>Search</span>
                         </button>
                     </form>
-
                 </div>
             </div>
         </>
