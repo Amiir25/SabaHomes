0cdbd2ec56d43651c0911b49f2b872c9be3cfeb5
Amiir25 <amirsadik636@gmail.com>
2025-09-26 15:16:36 +0300

    Update the the headlines of the hero section for small screen in Hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
 
     // Images for dynamic background
     const bgImages = [
-        'bg-image-1.jpg', 'bg-image-2.jpg', 'bg-image-3.jpg', 'bg-image-4.jpg',
-        'bg-image-5.jpg', 'bg-image-6.jpg', 'bg-image-7.jpg', 'bg-image-8.jpg',
+        // 'bg-image-1.jpg', 'bg-image-2.jpg', 'bg-image-3.jpg', 'bg-image-4.jpg',
+        // 'bg-image-5.jpg', 'bg-image-6.jpg', 'bg-image-7.jpg', 'bg-image-8.jpg',
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -34,18 +34,18 @@ const Hero = () => {
 
                 <div className="relative z-10 px-6 md:px-16 lg:px-24 xl:px-32 w-screen h-screen
                     bg-gradient-to-r from-white/95 via-white/80 to-white/0 text-gray-900">
-                    <div className="pt-45 w-fit text-center">
+                    <div className="pt-35 md:pt-45 w-fit text-center">
                         {/* <p className="md:text-3xl font-black">Homes for Every Stage of Life</p> */}
-                        <h1 className="md:text-7xl font-light mt-2 max-w-3xl [text-shadow:4px_4px_12px_rgba(0,0,0,0.8)]">
+                        <h1 className="text-3xl md:text-7xl font-light mt-2 max-w-3xl [text-shadow:4px_4px_12px_rgba(0,0,0,0.8)]">
                             Find Your Perfect Home in Addis Ababa!
                         </h1>
-                        <p className="max-w-2xl mt-4 text-3xl text-center mx-auto [text-shadow:1px_1px_2px_rgba(0,0,0,0.4)] tracking-wider">
+                        <p className="max-w-2xl mt-4 md:text-3xl text-center mx-auto [text-shadow:1px_1px_2px_rgba(0,0,0,0.4)] tracking-wider">
                         SabaHomes connects you to the finest apartments, villas, and offices across Addis Ababa.
                         </p>
                     </div>
 
                     {/* Form */}
-                    <form className='w-fit mt-6 bg-[rgba(255,255,255,0.8)] border text-gray-900 rounded-lg px-6 py-4 
+                    <form className='w-fit mt-6 bg-white/80 border text-gray-900 rounded-lg px-6 py-4 
                     flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
 
                         <div>
